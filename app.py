0d5153ce31917d424ba253d171c130b662445667
Karanjot786 <99573351+Karanjot786@users.noreply.github.com>
2022-07-08 11:22:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, request, jsonify
 from flask.templating import render_template
-from inshort import getNews
+from inshorts import getNews
 from flask_cors import CORS
+import markdown.extensions.fenced_code
+import markdown.extensions.codehilite
 from pygments.formatters import HtmlFormatter
-import os
+
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/', methods=['GET', 'POST'])
-
+@app.route('/')
 def home():
+    readme_file = open("README.md", "r")
+    md_template_string = markdown.markdown(
+        readme_file.read(), extensions=["fenced_code", "codehilite"])
+    formatter = HtmlFormatter(style="emacs",full=True,cssclass="codehilite")
+    css_string = formatter.get_style_defs()
+    md_css_string = "<style>" + css_string + "</style>"
+    
+    md_template = md_css_string + md_template_string
+    return md_template
+
+@app.route('/news', methods=['GET', 'POST'])
+
+def news():
     if request.method == 'POST':
         return jsonify(getNews(request.form['category']))
     elif request.method == 'GET':
@@ -18,3 +32,4 @@ def home():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(port = int(os.environ.get('PORT', 5000)),threaded=True)
+
